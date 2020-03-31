b5289c2bb694dfe24d9dcc4441ad89ddf91576cc
Luan Lima <luanyata@gmail.com>
2020-03-31 08:35:52 -0300

    :sparkles: (Bina) Identificador de chamada
    Criacao do evento que passa o numero da chamada para o frontend

--- src/events/index.js
+++ src/events/index.js
@@ -1,6 +1,7 @@
 const { EventEmitter } = require('events')
 
 const Events = {
+    handleCallState: new EventEmitter(),
     handleWsState: new EventEmitter(),
     handleVerto: new EventEmitter(),
 }
